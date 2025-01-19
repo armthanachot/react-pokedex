0adc026f03857427839e3ee4e21b4d31e67fa2aa
Thanachot Tesjaroen <arm1997a@gmail.com>
2025-01-19 14:01:02 +0700

    -

--- src/pages/PokemonCard.tsx
+++ src/pages/PokemonCard.tsx
@@ -6,50 +6,71 @@ import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import { Box, IconButton } from '@mui/material';
 import SimpleDialog from '../components/Dialog';
-import { AutoAwesome, PlayArrow, FlipCameraIos } from '@mui/icons-material';
+import { AutoAwesome, PlayArrow, FlipCameraIos, LocalFireDepartment, NoteAdd } from '@mui/icons-material';
 import { defaultImg, pokemonImage, showDownImage } from '../../dto/pokemon';
 
 // change img to object of normal and shiny (contain front and back)
 export default function PokemonCard({ defaultImage, showDownImage, name, types, voice, no }: { defaultImage: defaultImg, showDownImage: showDownImage, name: string, types: string[], voice?: string, no?: number }) {
 
-    const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialogState, setDialogState] = useState({
+        animationDialog: false,
+        noteDialog: false
+    });
 
     const [pokemonImg, setPokemonImg] = useState<pokemonImage>({ front: defaultImage.front_default, back: defaultImage.back_default, show: defaultImage.front_default });
 
     const [animationSrc, setAnimationSrc] = useState<string>(showDownImage.front_default);
 
-    const [shiny, setShiny] = useState(false);
-
-    const [flip, setFlip] = useState(false);
+    const [pokemonFunctionState, setPokemonFunctionState] = useState({
+        flip: false,
+        shiny: false
+    });
 
     const playAnimation = () => {
-        setDialogOpen(true);
+        setDialogState({
+            animationDialog: true,
+            noteDialog: false
+        });
     }
 
     const convertToshiny = () => {
-        setShiny(!shiny);
+        setPokemonFunctionState(prevState => ({
+            ...prevState,
+            shiny: !prevState.shiny
+        }));
     }
 
     useEffect(() => {
-        setAnimationSrc(shiny ? showDownImage.front_shiny : showDownImage.front_default);
-        return setPokemonImg(shiny ? { front: defaultImage.front_shiny, back: defaultImage.back_shiny, show: defaultImage.front_shiny } : { front: defaultImage.front_default, back: defaultImage.back_default, show: defaultImage.front_default });
-    }, [shiny])
+        setAnimationSrc(pokemonFunctionState.shiny ? showDownImage.front_shiny : showDownImage.front_default);
+        setPokemonImg(pokemonFunctionState.shiny ? { front: defaultImage.front_shiny, back: defaultImage.back_shiny, show: defaultImage.front_shiny } : { front: defaultImage.front_default, back: defaultImage.back_default, show: defaultImage.front_default });
+    }, [pokemonFunctionState.shiny])
 
     const flipPokemon = () => {
-        setFlip(!flip);
+        setPokemonFunctionState(prevState => ({
+            ...prevState,
+            flip: !prevState.flip,
+        }));
     }
     useEffect(() => {
-        return setPokemonImg(flip ? { front: pokemonImg.front, back: pokemonImg.front, show: pokemonImg.back } : { front: pokemonImg.front, back: pokemonImg.back, show: pokemonImg.front });
-    }, [flip])
+        setPokemonImg((prevState)=>{
+            return {
+                ...prevState,
+                show: pokemonFunctionState.flip ? prevState.back : prevState.front
+            }
+        });
+    }, [pokemonFunctionState.flip])
 
-    const onCloseDialog = () => {
-        setDialogOpen(false);
+    const onCloseAnimationDialog = () => {
+        setDialogState({
+            animationDialog: false,
+            noteDialog: false
+        });
     }
 
     return (
         <Card>
             <CardActionArea>
-                <Typography variant="body2" color="text.secondary" sx={{ padding: 2 }}>
+                <Typography variant="body2" color="black" sx={{ padding: 2 }}>
                     {no}.
                 </Typography>
                 <CardContent sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
@@ -78,10 +99,18 @@ export default function PokemonCard({ defaultImage, showDownImage, name, types, 
                                         <AutoAwesome fontSize='large' htmlColor='#eef11c' />
                                     </IconButton>
                                 </Box>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                                    {/* <IconButton onClick={() => playAnimation()}>
+                                        <LocalFireDepartment fontSize='large' htmlColor='#ffa601' />
+                                    </IconButton> */}
+                                    <IconButton onClick={() => convertToshiny()}>
+                                        <NoteAdd fontSize='large' htmlColor='#ce6004' />
+                                    </IconButton>
+                                </Box>
                                 {
-                                    dialogOpen &&
+                                    dialogState.animationDialog &&
                                     SimpleDialog({
-                                        open: dialogOpen, title: name, content: (<Card sx={{}}>
+                                        open: dialogState.animationDialog, title: name, content: (<Card sx={{}}>
                                             <CardActionArea>
                                                 <CardContent>
                                                     <CardMedia
@@ -92,7 +121,7 @@ export default function PokemonCard({ defaultImage, showDownImage, name, types, 
                                                 </CardContent>
                                             </CardActionArea>
                                         </Card>)
-                                        , onCloseDialog: () => onCloseDialog(),
+                                        , onCloseDialog: () => onCloseAnimationDialog(),
                                         voice: voice
                                     })
                                 }
